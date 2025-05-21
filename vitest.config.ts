70a51a6e0762450f637e942198e28075694ce9e0
Melody Universe <melody@computost.com>
2025-05-20 21:41:58 -0700

    add back end test

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,28 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    environment: "jsdom",
-    setupFiles: ["vitest/cleanup-after-each.ts", "vitest/register-jest-dom.ts"],
+    projects: [
+      {
+        extends: true,
+        test: {
+          environment: "jsdom",
+          include: ["app/**/*.client.test.{ts,tsx}"],
+          name: "client",
+          setupFiles: [
+            "vitest/cleanup-after-each.ts",
+            "vitest/register-jest-dom.ts",
+          ],
+        },
+      },
+      {
+        extends: true,
+        test: {
+          environment: "node",
+          include: ["app/**/*.server.test.ts"],
+          name: "server",
+        },
+      },
+    ],
+    setupFiles: ["vitest/cleanup-after-each.ts"],
   },
 });
