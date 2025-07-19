14a16799be9bf856d25ecd644c32f3fd5299c768
Melody Universe <melody@computost.com>
2025-07-19 12:19:01 -0700

    use ref

--- app/components/Parallax.tsx
+++ app/components/Parallax.tsx
@@ -1,92 +1,106 @@
-import { type ReactNode, useEffect, useState } from "react";
+import { type ReactNode, useEffect, useRef } from "react";
+
+type Layer = {
+  definition: LayerDefinition;
+  tiles: Tile[][];
+};
+
+type LayerDefinition = {
+  brightness: number;
+  density: number;
+  index: number;
+  radius: number;
+};
+
+type Tile = {
+  offset: number;
+  stars: Star[];
+};
 
 export function Parallax(): ReactNode {
-  const [[screenWidth, screenHeight], setScreenSize] = useState<
-    [number, number]
-  >([0, 0]);
+  const graphicsRef = useRef<SVGGElement>(null);
 
-  useEffect(function manageScreenSize() {
-    refreshScreenSize();
-    window.addEventListener("resize", refreshScreenSize);
+  const stateRef = useRef<{
+    layers: Layer[];
+    visible: { height: number; width: number };
+  }>({
+    layers: layerDefinitions.map((definition) => ({ definition, tiles: [] })),
+    visible: { height: 0, width: 0 },
+  });
+  const { layers, visible } = stateRef.current;
 
-    return () => window.removeEventListener("resize", refreshScreenSize);
+  useEffect(function renderStars() {
+    let handle = requestAnimationFrame(handleNewFrame);
+    // let startTime = performance.now();
 
-    function refreshScreenSize() {
-      setScreenSize([document.body.clientWidth, document.body.clientHeight]);
+    function handleNewFrame(/* endTime: number */) {
+      // const elapsedTime = endTime - startTime;
+
+      render();
+
+      // startTime = endTime;
+      handle = requestAnimationFrame(handleNewFrame);
+
+      function render() {
+        const graphics = graphicsRef.current;
+        if (!graphics) {
+          return;
+        }
+
+        const screenWidth = document.body.clientWidth;
+        const screenHeight = document.body.clientHeight;
+
+        const initialHeight = visible.height;
+        const initialWidth = visible.width;
+
+        addMissingTiles(graphics);
+
+        function addMissingTiles(graphics: SVGGElement) {
+          if (initialHeight * tileSize <= screenHeight) {
+            visible.height = Math.ceil(1 + screenHeight / tileSize);
+
+            for (const layer of layers) {
+              for (let tileY = initialHeight; tileY < visible.height; tileY++) {
+                layer.tiles.push([]);
+
+                for (let tileX = 0; tileX < initialWidth; tileX++) {
+                  layer.tiles[tileY].push(
+                    createTile(layer.definition, graphics, tileX, tileY),
+                  );
+                }
+              }
+            }
+          }
+
+          if (initialWidth * tileSize <= screenWidth) {
+            visible.width = Math.ceil(1 + screenWidth / tileSize);
+
+            for (const layer of layers) {
+              for (let tileY = 0; tileY < visible.height; tileY++) {
+                for (let tileX = initialWidth; tileX < visible.width; tileX++) {
+                  layer.tiles[tileY].push(
+                    createTile(layer.definition, graphics, tileX, tileY),
+                  );
+                }
+              }
+            }
+          }
+        }
+      }
     }
+
+    return () => cancelAnimationFrame(handle);
   }, []);
-
-  const [starMap, setStarMap] = useState<
-    {
-      animationDelay: number;
-      color: number;
-      layerIndex: number;
-      x: number;
-      y: number;
-    }[][][]
-  >([]);
-
-  useEffect(() => {
-    setStarMap((map) => {
-      if (
-        map.length > 0 &&
-        map[0].length * tileSize > screenWidth &&
-        map.length * tileSize > screenHeight
-      ) {
-        return map;
-      }
-
-      const newMap = map.map((row) => [...row]);
-
-      if (map.length * tileSize <= screenHeight) {
-        for (let y = map.length; y <= screenHeight / tileSize; y++) {
-          newMap.push([]);
-          for (let x = 0; x < newMap[0].length; x++) {
-            newMap[y].push([]);
-            for (let i = 0; i < layers.length; i++) {
-              newMap[y][x].push({
-                animationDelay: Math.random() * 2,
-                color: Math.floor(Math.random() * colors),
-                layerIndex: i,
-                x: (x + Math.random()) * tileSize,
-                y: (y + Math.random()) * tileSize,
-              });
-            }
-          }
-        }
-      }
-
-      if (newMap[0].length * tileSize <= screenWidth) {
-        for (let x = map[0]?.length ?? 0; x <= screenWidth / tileSize; x++) {
-          for (let y = 0; y < newMap.length; y++) {
-            newMap[y].push([]);
-            for (let i = 0; i < layers.length; i++) {
-              newMap[y][x].push({
-                animationDelay: Math.random() * 2,
-                color: Math.floor(Math.random() * colors),
-                layerIndex: i,
-                x: (x + Math.random()) * tileSize,
-                y: (y + Math.random()) * tileSize,
-              });
-            }
-          }
-        }
-      }
-
-      return newMap;
-    });
-  }, [screenWidth, screenHeight]);
 
   return (
     <div className="absolute top-0 left-0">
       <div className="h-screen w-screen overflow-hidden">
         <svg
-          height={screenHeight}
-          width={screenWidth}
+          className="h-full min-h-screen w-full min-w-screen"
           xmlns="http://www.w3.org/2000/svg"
         >
           <defs>
-            {layers.map((layer, layerIndex) =>
+            {layerDefinitions.map((layer, layerIndex) =>
               Array.from({ length: colors }).map((_, colorIndex) => (
                 <radialGradient
                   cx={0.5}
@@ -106,38 +120,75 @@ export function Parallax(): ReactNode {
               )),
             )}
           </defs>
-          <g>
-            {starMap.flatMap((row, rowIndex) =>
-              row.flatMap((column, columnIndex) =>
-                column.map(({ animationDelay, color, layerIndex, x, y }, i) => (
-                  <circle
-                    className="animate-pulse"
-                    cx={x}
-                    cy={y}
-                    fill={`url(#layer-${layerIndex}-color-${color})`}
-                    key={`${rowIndex}_${columnIndex}_${i}`}
-                    r={layers[layerIndex].radius}
-                    style={{ animationDelay: `${animationDelay}s` }}
-                  />
-                )),
-              ),
-            )}
-          </g>
+          <g ref={graphicsRef} />
         </svg>
       </div>
     </div>
   );
 }
 
-const layers: { brightness: number; density: number; radius: number }[] = [
-  { brightness: 1, density: 4, radius: 2.5 },
-  { brightness: 0.5, density: 6, radius: 2 },
-  { brightness: 0.25, density: 8, radius: 1.5 },
-];
+const layerDefinitions: LayerDefinition[] = [
+  { brightness: 1, density: 1, radius: 2.5 },
+  { brightness: 0.5, density: 2, radius: 2 },
+  { brightness: 0.25, density: 3, radius: 1.5 },
+].map((definition, index) => ({ ...definition, index }));
 
 const colors = 10;
 
 const tileSize = 100;
+
+type Star = {
+  animationDelay: number;
+  circle: SVGCircleElement;
+  color: number;
+  x: number;
+  y: number;
+};
+
+function createTile(
+  layerDefinition: LayerDefinition,
+  graphics: SVGGElement,
+  tileX: number,
+  tileY: number,
+): Tile {
+  return {
+    offset: 0,
+    stars: Array.from({
+      length: layerDefinition.density,
+    }).map(() => createStar()),
+  };
+
+  function createStar(): Star {
+    const animationDelay = Math.random() * 2;
+    const color = Math.floor(Math.random() * colors);
+    const x = (tileX + Math.random()) * tileSize;
+    const y = (tileY + Math.random()) * tileSize;
+
+    const circle = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle",
+    );
+    circle.classList.add("animate-pulse");
+    circle.setAttribute("cx", x.toString());
+    circle.setAttribute("cy", y.toString());
+    circle.setAttribute(
+      "fill",
+      `url(#layer-${layerDefinition.index}-color-${color})`,
+    );
+    circle.setAttribute("r", layerDefinition.radius.toString());
+    circle.style.animationDelay = `${animationDelay}s`;
+
+    graphics.append(circle);
+
+    return {
+      animationDelay,
+      circle,
+      color,
+      x,
+      y,
+    };
+  }
+}
 
 function hslToHex(hue: number, saturation: number, lightness: number) {
   lightness /= 100;
